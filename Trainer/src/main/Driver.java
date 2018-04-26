bc7183a8df0ef515963a79a0e0287eb6b73fa282
Remi <pinoit.remi@gmail.com>
2018-04-26 23:01:57 +0200

    begin tutorial

---
+++ Trainer/src/main/Driver.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class Driver
+{
+	private static final int width = 500;
+	private static final int height = 500;
+	
+	public static void main(String[] args)
+	{
+		long	start;
+		long	end;	
+		File image = new File("Image.png");
+		BufferedImage buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		
+		start = System.nanoTime();
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				buffer.setRGB(x, y, 1000);
+			}
+		}
+		try
+		{
+			ImageIO.write(buffer, "PNG", image);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Couldn't write image.");
+			System.exit(1);
+		}
+		
+		end = System.nanoTime();
+		System.out.println("Loop time: " + (end - start) / 1000000000.0F);
+	}
+}
